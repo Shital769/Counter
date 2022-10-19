9b80dd09d6a6305161ee79150841f86c8532add1
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-10-19 21:54:17 +1100

    Started to code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <React.Fragment>
+      <React.Fragment >
         <h1>Hello World</h1>
         <button>Increment</button>
       </React.Fragment>

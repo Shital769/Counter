0c13ade3308cd6907bbe220a5df92b694db5cc05
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-10-20 01:55:49 +1100

    rendered lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+//defining a method for increment
+handleIncrement() {
+    console.log("Increment Clicked");
+}
+
   render() {
     return (
-      <React.Fragment >
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        {/* we can put any valid javascript expression inside this curly braces down below. */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {/* mapping string tag into plain javaScript object */}
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+   return classes;
+  }
+
+ formatCount() {
+   const { count } = this.state;
+   return count === 0 ? "Zero" : count;
+ }
 }
 
 export default Counter;
+
+
+
+ 
